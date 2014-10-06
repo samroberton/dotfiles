2ceca2b21d9b4b1bc58f7817d505818cb5fa84e4
Sam Roberton <sam@samroberton.com>
2014-10-06 15:38:24 +1100

    Add *print-length* for lein project REPLs.

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,2 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.7.0"]]}
+{:user {:plugins [[cider/cider-nrepl "0.7.0"]]
+        :repl-options {:init (set! *print-length* 200)}}
  :repl {:resource-paths ["/usr/lib/jvm/java-8-oracle/lib/tools.jar"]}}
