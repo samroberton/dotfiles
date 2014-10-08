f00db7f223a4ed97af737a31487b7d9fc07e2048
Sam Roberton <sam@samroberton.com>
2014-10-09 00:54:47 +1100

    Clojure lein repl setup: set *print-level* as well as *print-length*.

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.7.0"]]
-        :repl-options {:init (set! *print-length* 200)}}
+        :repl-options {:init (do
+                               (set! *print-length* 200)
+                               (set! *print-level* 20))}}
  :repl {:resource-paths ["/usr/lib/jvm/java-8-oracle/lib/tools.jar"]}}
