be0bda0e3440280fa0a9e3b12b0220aec9c3ee51
Sam Roberton <sam@samroberton.com>
2014-12-04 08:26:59 +1100

    Some more useful lein plugins.

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,4 +1,8 @@
-{:user {:plugins [[cider/cider-nrepl "0.7.0"]]
+{:user {:plugins [[cider/cider-nrepl "0.7.0"]
+                  [lein-try "0.3.2"]
+                  [lein-pprint "1.1.1"]
+                  [lein-ancient "0.4.4"]
+                  [lein-bikeshed "0.1.3"]]
         :repl-options {:init (do
                                (set! *print-length* 200)
                                (set! *print-level* 20))}}
