f067ac11cacd1ee6446c99b1e88bde13c6aa81c6
Sam Roberton <sam@samroberton.com>
2015-06-24 22:42:45 +1000

    lein: CIDER 0.9.0.

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-20150502.155350-34"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0"]
                   [lein-try "0.4.3"]
                   [lein-pprint "1.1.2"]
                   [lein-ancient "0.6.7"] ; `lein ancient profiles`
@@ -10,8 +10,7 @@
                   [refactor-nrepl "1.0.5"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [org.clojure/tools.reader "0.9.2"]
-                       [jonase/kibit "0.1.2"]
-                       [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
+                       [jonase/kibit "0.1.2"]]
         :repl-options {:init (do
                                (set! *print-length* 200)
                                (set! *print-level* 20))}}}
