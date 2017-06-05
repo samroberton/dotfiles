525beea874f10a7953dee2a10b893b5a673fc992
Sam Roberton <sam@samroberton.com>
2017-06-06 07:51:50 +1000

    emacs: big-mode/lil-mode

--- emacs/init.el
+++ emacs/init.el
@@ -300,5 +300,27 @@
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 
+(define-globalized-minor-mode
+  global-text-scale-mode
+  text-scale-mode
+  (lambda () (text-scale-mode 1)))
+
+(defun global-text-scale-adjust (inc)
+  (interactive)
+  (text-scale-set 1)
+  (kill-local-variable 'text-scale-mode-amount)
+  (setq-default text-scale-mode-amount (+ text-scale-mode-amount inc))
+  (global-text-scale-mode 1))
+
+(defun big-mode ()
+  (interactive)
+  (text-scale-set 1)
+  (global-text-scale-adjust 1))
+
+(defun lil-mode ()
+  (interactive)
+  (text-scale-set 1)
+  (global-text-scale-adjust -1))
+
 
 (message "init.el loaded in %fs" (- (float-time) *emacs-load-start*))
