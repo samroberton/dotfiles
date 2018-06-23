3f6ad1ad724de04eb98cfa22d0da5243f59c9209
Sam Roberton <sam@samroberton.com>
2018-06-23 17:51:40 +1000

    Convert Emacs `init.el` to `use-package`

--- emacs/init.el
+++ emacs/init.el
@@ -4,7 +4,8 @@
 (setq debug-on-error nil)
 
 
-;;;; General options -----------------------------------------------------------
+
+;;;; Keybindings ---------------------------------------------------------------
 
 (global-unset-key (kbd "C-x c"))        ; let's not accidentally kill Emacs
 (global-unset-key (kbd "C-x C-c"))      ; let's not accidentally kill Emacs
@@ -12,8 +13,40 @@
 
 (global-set-key (kbd "C-c f") 'toggle-frame-fullscreen)
 
-;; Allows us to use `case'
-(require 'cl)
+;; 'C-h C' describes coding system
+(global-set-key (kbd "C-c C") 'set-buffer-file-coding-system)
+
+
+(global-set-key (kbd "C-w") 'backward-kill-word) ; make C-w match readline behaviour
+(global-set-key (kbd "C-x C-k") 'kill-region)
+
+(global-set-key "\C-cb" 'bury-buffer)
+
+;; Searches might as well be regexps.
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+
+(global-set-key (kbd "M-/") 'company-complete)
+
+(global-set-key (kbd "C-x w") 'delete-trailing-whitespace)
+
+(global-set-key (kbd "C-x |") 'align-regexp)
+
+
+
+;;;; Registers -----------------------------------------------------------------
+
+;; usage:  C-x r j <register-identifier>  ('register jump')
+;; from http://www.emacswiki.org/cgi-bin/wiki/EmacsNiftyTricks
+(set-register ?i `(file . ,(concat user-emacs-directory "init.el")))
+(set-register ?c `(file . ,(concat user-emacs-directory "cheat-sheet.txt")))
+(set-register ?t '(file . "~/todo.txt"))
+
+
+
+;;;; General options -----------------------------------------------------------
+
+(require 'cl)                           ; provides `case'
 
 (set-language-environment "UTF-8")      ; default is "English"
 (prefer-coding-system 'utf-8-unix)
@@ -21,14 +54,14 @@
 (setq inhibit-startup-message t
       initial-scratch-message nil)
 
-(setq-default indicate-empty-lines t
+(setq-default indicate-empty-lines     t
               show-trailing-whitespace t
-              indent-tabs-mode nil
-              tab-width 4
-              require-final-newline t
-              save-place t
-              fill-column 80
-              comment-column 40)
+              indent-tabs-mode         nil
+              tab-width                4
+              require-final-newline    t
+              save-place               t
+              fill-column              80
+              comment-column           40)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -42,16 +75,16 @@
 
 (add-hook 'text-mode-hook 'visual-line-mode)
 
-(setq mouse-yank-at-point t
-      x-select-enable-clipboard t
-      x-select-enable-primary t
+(setq mouse-yank-at-point                 t
+      select-enable-clipboard             t
+      select-enable-primary               t
       save-interprogram-paste-before-kill t
-      echo-keystrokes 0.1
-      vc-follow-symlinks t)
+      echo-keystrokes                     0.1
+      vc-follow-symlinks                  t)
 
 ;; Avoid trying to ping some machine in Hong Kong just because you typed
 ;; something.hk: https://github.com/technomancy/emacs-starter-kit/issues/39
-(setq ffap-machine-p-known 'reject)
+(setq-default ffap-machine-p-known 'reject)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -59,19 +92,30 @@
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 
+
 (setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file)
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 ;; Use Source Code Pro when available:
 ;; https://github.com/adobe-fonts/source-code-pro
 (when (x-list-fonts "Source Code Pro Light")
-  (set-default-font "Source Code Pro Light")
+  (set-frame-font "Source Code Pro Light")
   (set-face-attribute 'default nil :height (case system-type
                                              (darwin     120)
                                              (windows-nt 120)
                                              (otherwise  100))))
 
-(setq wdired-allow-to-change-permissions t)
+(setq-default wdired-allow-to-change-permissions t)
+
+
+
+;;;; Emacsclient ---------------------------------------------------------------
+
+(require 'server)
+(unless (server-running-p)
+  (add-hook 'after-init-hook 'server-start))
+
 
 
 ;;;; Platform-specific customisations ------------------------------------------
@@ -79,7 +123,7 @@
 ;; PATH on Mac OS X missing ~/bin because it's not launched from bash
 ;; http://stackoverflow.com/a/15728130
 (when (equal system-type 'darwin)
-  (setq explicit-bash-args (list "--login" "-i"))
+  (setq-default explicit-bash-args (list "--login" "-i"))
   (let ((path-from-shell
          (shell-command-to-string "$SHELL -i -l -c 'echo $PATH'")))
     (setenv "PATH" path-from-shell)
@@ -92,6 +136,7 @@
 
 
 ;;;; Auto-saves ----------------------------------------------------------------
+
 (defvar --backup-directory (concat user-emacs-directory "backups"))
 (if (not (file-exists-p --backup-directory))
     (make-directory --backup-directory t))
@@ -106,216 +151,224 @@
       kept-old-versions 2)              ; keep the original 2
 
 
-;;;; Emacs 24 Package setup ----------------------------------------------------
+
+;;;; Emacs Package setup -------------------------------------------------------
+
 (require 'package)
+
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 ;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
 (package-initialize)
 
-
-(add-hook 'after-init-hook 'global-company-mode)
-
-(setq magit-last-seen-setup-instructions "1.4.0")
-
-
-;;;; Helm ----------------------------------------------------------------------
-
-(require 'helm-config)
-
-(global-set-key "\C-ch" 'helm-command-prefix)
-
-(global-set-key "\M-x" 'helm-M-x)
-(global-set-key "\M-y" 'helm-show-kill-ring)
-(global-set-key "\C-xb" 'helm-mini)
-(global-set-key "\C-x\C-f" 'helm-find-files)
-(global-set-key "\C-cho" 'helm-occur)
-
-(with-eval-after-load 'helm
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-  (define-key helm-map "\C-i" 'helm-execute-persistent-action) ; make TAB work in the terminal
-  (define-key helm-map "\C-z" 'helm-select-action))
-
-(add-hook 'shell-mode-hook
-          #'(lambda ()
-              (define-key shell-mode-map "\C-c\C-l" 'helm-comint-input-ring)))
-(define-key minibuffer-local-map "\C-c\C-l" 'helm-minibuffer-history)
-
-(setq helm-split-window-in-side-p           t ; open helm buffer in current window
-      helm-move-to-line-cycle-in-source     t
-      helm-ff-file-name-history-use-recentf t
-      helm-M-x-fuzzy-match                  t
-      helm-buffers-fuzzy-matching           t
-      helm-recentf-fuzzy-match              t
-      helm-apropos-fuzzy-match              t)
-
-(helm-mode 1)
-
-
-;;;; Projectile ----------------------------------------------------------------
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(require 'helm-projectile)
-(helm-projectile-on)
-
-
-;;;; Uniquify ------------------------------------------------------------------
-(require 'uniquify)                     ; make buffer names unique
-(setq uniquify-buffer-name-style 'reverse
-      uniquify-after-kill-buffer-p t
-      uniquify-ignore-buffers-re "^\\*") ; don't screw with special buffers
-
-
-;;;; Paredit -------------------------------------------------------------------
-(when (require 'paredit nil 'noerror)
-  (autoload 'enable-paredit-mode "paredit" t)
-  (eval-after-load 'paredit
-    '(progn
-       (define-key paredit-mode-map "{" 'paredit-open-curly)
-       (define-key paredit-mode-map "}" 'paredit-close-curly)
-       (define-key paredit-mode-map "\C-w" 'backward-kill-sexp)))
-
-  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'clojure-mode-hook 'enable-paredit-mode)
-  (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
-  (add-hook 'slime-mode-hook 'enable-paredit-mode)
-  (add-hook 'slime-repl-mode-hook 'enable-paredit-mode))
-
-(setq cider-repl-display-help-banner nil)
-
-
-;;;; Clj-refactor --------------------------------------------------------------
-(require 'clj-refactor)
-(add-hook 'clojure-mode-hook (lambda ()
-                               (eldoc-mode 1)
-                               ;;(clj-refactor-mode 1)
-                               (yas/minor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-a")))
-
-(setq cider-repl-display-help-banner nil)
-
-
-;;;; Avy mode ------------------------------------------------------------------
-(require 'avy)
-(global-set-key (kbd "C-c j") 'avy-goto-word-1)
-
-
-;;;; Keybindings ---------------------------------------------------------------
-
-;; 'C-h C' describes coding system
-(global-set-key "\C-cC" 'set-buffer-file-coding-system)
-
-
-;; use C-w to delete back a word to match the shortcut in the shell
-;; Rebind kill-region (normally C-w) to C-x C-k
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-
-(global-set-key "\C-cb" 'bury-buffer)
-
-;; searches might as well be regexps
-(global-set-key "\C-s" 'isearch-forward-regexp)
-(global-set-key "\C-r" 'isearch-backward-regexp)
-
-(global-set-key "\M-/" 'company-complete)
-
-(global-set-key "\C-m" 'newline-and-indent)
-
-(global-set-key "\C-xw" 'delete-trailing-whitespace)
-
-(global-set-key "\C-x|" 'align-regexp)
-
-(global-set-key "\C-cg" 'magit-status)
-(global-set-key "\C-cG" 'helm-projectile-ag)
-
-(setq helm-ag-use-agignore t)
-
-;;;; Registers -----------------------------------------------------------------
-;; usage:  C-x r j <register-identifier>  ('register jump')
-;; from http://www.emacswiki.org/cgi-bin/wiki/EmacsNiftyTricks
-(set-register ?i `(file . ,(concat user-emacs-directory "init.el")))
-(set-register ?c `(file . ,(concat user-emacs-directory "cheat-sheet.txt")))
-(set-register ?t '(file . "~/todo.txt"))
-
-
-;;;; Emacsclient ---------------------------------------------------------------
-(require 'server)
-(unless (server-running-p)
-  (add-hook 'after-init-hook 'server-start))
-
-
-;;;; Themes --------------------------------------------------------------------
-(with-demoted-errors
-  (load-theme 'junio))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 
 
-;;;; .emacs config for encouraging staying under 100 cols
-;; (defvar no-whitespace-highlighting-modes
-;;   '(Man-mode
-;;     calendar-mode
-;;     ...))
+;;;; Packages ------------------------------------------------------------------
 
-;; (use-package highlight-beyond-fill-column
-;;   :config (add-hook 'font-lock-mode-hook
-;;             (lambda ()
-;;               (unless (member major-mode no-whitespace-highlighting-modes)
-;;                 (highlight-beyond-fill-column)))))
+(use-package helm
+  :ensure t
+  :bind   (("M-x"     . helm-M-x)
+           ("M-y"     . helm-show-kill-ring)
+           ("C-x b"   . helm-mini)
+           ("C-x C-f" . helm-find-files)
+           ("C-c h o" . helm-occur)
 
-(require 'whitespace)
-(setq whitespace-line-column 100)
-(setq whitespace-style '(face lines-tail))
+           :map helm-map
+           ("[tab]" . helm-execute-persistent-action)
+           ("C-i"   . helm-execute-persistent-action)
+           ("C-z"   . helm-select-action))
 
-(add-hook 'prog-mode-hook 'whitespace-mode)
-
-
-;;;; Haskell -------------------------------------------------------------------
-
-;(autoload 'ghc-init "ghc" nil t)
-;(autoload 'ghc-debug "ghc" nil t)
-;(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
-(setq haskell-stylish-on-save t)
-
-(require 'intero)
-(require 'flycheck)
-(with-eval-after-load 'intero
-  (add-hook 'haskell-mode-hook
-            (lambda ()
-              (flycheck-mode)))
-  (with-eval-after-load 'flycheck
-    (flycheck-add-next-checker 'intero '(warning . haskell-hlint))))
+  :config
+  (setq helm-split-window-inside-p        t ; open helm buffer in current window
+        helm-move-to-line-cycle-in-source t)
+  (setq-default helm-ff-file-name-history-use-recentf t
+                helm-M-x-fuzzy-match                  t
+                helm-buffers-fuzzy-matching           t
+                helm-recentf-fuzzy-match              t
+                helm-apropos-fuzzy-match              t)
+  (helm-mode 1))
 
 
-;;;; Multiple cursors ----------------------------------------------------------
 
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(use-package projectile
+  :ensure   t
+  :commands (projectile-find-file projectile-switch-project)
+  :config
+  (projectile-global-mode)
+  (setq projectile-completion-system 'helm))
 
 
-(define-globalized-minor-mode
-  global-text-scale-mode
-  text-scale-mode
-  (lambda () (text-scale-mode 1)))
 
-(defun global-text-scale-adjust (inc)
-  (interactive)
-  (text-scale-set 1)
-  (kill-local-variable 'text-scale-mode-amount)
-  (setq-default text-scale-mode-amount (+ text-scale-mode-amount inc))
-  (global-text-scale-mode 1))
+(use-package helm-projectile
+  :ensure t
+  :config (helm-projectile-on))
 
-(defun big-mode ()
-  (interactive)
-  (text-scale-set 1)
-  (global-text-scale-adjust 1))
 
-(defun lil-mode ()
-  (interactive)
-  (text-scale-set 1)
-  (global-text-scale-adjust -1))
+(use-package helm-ag
+  :ensure   t
+  :commands (helm-ag helm-projectile-ag)
+  :bind     ("C-c G" . helm-projectile-ag))
 
+
+
+(use-package uniquify                   ; make buffer names unique
+  :ensure nil
+  :config
+  (setq uniquify-buffer-name-style   'reverse
+        uniquify-ignore-buffers-re   "^\\*" ; don't screw with special buffers
+        uniquify-after-kill-buffer-p t))
+
+
+
+(use-package paredit
+  :ensure t
+  :bind   (:map paredit-mode-map
+                ("{"   . paredit-open-curly)
+                ("}"   . paredit-close-curly)
+                ("C-w" . backward-kill-sexp))
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+  (add-hook 'list-mode-hook #'paredit-mode)
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'paredit-mode)
+  (add-hook 'slime-mode-hook #'paredit-mode)
+  (add-hook 'slime-repl-mode #'paredit-mode))
+
+
+
+(use-package cider
+  :ensure t
+  :config
+  (setq cider-repl-display-help-banner nil))
+
+
+
+(use-package avy
+  :ensure t
+  :bind   ("C-c j" . avy-goto-word-1))
+
+
+
+(use-package magit
+  :ensure t
+  :bind   (("C-c g" . magit-status))
+  :config
+  (setq-default magit-last-seen-setup-instructions "1.4.0"))
+
+
+
+(use-package git-timemachine
+  :ensure t)
+
+
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode))
+
+
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+
+
+(use-package multiple-cursors
+  :ensure t
+  :bind   (("C-S-c C-S-c" . mc/edit-lines)
+           ("C->"         . mc/mark-next-like-this)
+           ("C-<"         . mc/mark-previous-like-this)
+           ("C-c C-<"     . mc/mark-all-like-this)))
+
+
+
+(use-package whitespace
+  :ensure t
+
+  :init
+  (add-hook 'prog-mode-hook #'whitespace-mode)
+  (add-hook 'text-mode-hook #'whitespace-mode)
+
+  :config
+  (setq whitespace-line-column 100
+        whitespace-style       '(face lines-tail)))
+
+
+
+(use-package haskell-mode
+  :ensure   t
+  :commands haskell-mode
+  :config
+  (setq haskell-stylish-on-save t))
+
+
+
+(use-package dante
+  :ensure   t
+  :after    haskell-mode
+  :commands dante-mode
+
+  :init
+  (add-hook 'haskell-mode-hook 'dante-mode)
+  (add-hook 'haskell-mode-hook 'flycheck-mode)
+
+  :config
+  (add-hook 'dante-mode-hook
+            '(lambda ()
+               (flycheck-add-next-checker 'haskell-dante
+                                          '(warning . haskell-hlint)))))
+
+
+
+(use-package sublime-themes             ; Not in melpa-stable
+  :ensure nil
+  :config
+  (with-demoted-errors                  ; Don't error when no window system
+      (load-theme 'junio)))
+
+
+
+(use-package default-text-scale
+  :ensure t
+  :bind   (("C-M-=" . default-text-scale-increase)
+           ("C-M--" . default-text-scale-decrease)
+           ;; Note: doesn't yet exist in melpa-stable version.
+           ("C-M-0" . default-text-scale-reset)))
+
+
+(use-package markdown-mode
+  :ensure t)
+
+
+
+(use-package yaml-mode
+  :ensure t)
+
+
+
+(use-package expand-region
+  :ensure t
+  :bind   (("C-=" . er/expand-region)
+           ("C--" . er/contract-region)))
+
+
+
+(use-package undo-tree                  ; "C-x u" to visualize
+  :ensure t
+  :bind   (("C-?" . undo-tree-redo))
+  :config
+  (global-undo-tree-mode 1))
+
+
+
+
+;;;; The End -------------------------------------------------------------------
 
 (message "init.el loaded in %fs" (- (float-time) *emacs-load-start*))
