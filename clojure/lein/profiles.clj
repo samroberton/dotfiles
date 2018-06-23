e39cba346fbbae56b9c7b578f5e58edd01e4a364
Sam Roberton <sam@samroberton.com>
2018-06-23 22:46:34 +1000

    Remove CIDER config from `.lein/profiles.clj`

--- clojure/lein/profiles.clj
+++ clojure/lein/profiles.clj
@@ -1,6 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.14.0" :exclusions [org.clojure/clojure org.clojure/tools.reader]]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [org.clojure/tools.reader "1.0.0-beta4" :exclusions [org.clojure/clojure]]]
-        :repl-options {:init (do
+{:user {:repl-options {:init (do
                                (set! *print-length* 200)
                                (set! *print-level* 20))}}}
